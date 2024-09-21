41e91ca4776bfe3c43898c314b0c6e5727beaffe
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-09-21 14:06:53 +0100

    .

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 import discord
-
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
+
+load_dotenv()
+TOKEN: Final[str] = os.getenv('TOKEN')
 
 client = discord.Client(intents=intents)
 
