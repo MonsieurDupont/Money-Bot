6c9c55348faa27220b7c22a5695f1a818732e7a9
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-09-21 20:02:46 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,16 @@
-import discord
+import discord 
+import mysql.connector
 from discord.ext import commands
 from discord.interactions import Interaction
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+conn = mysql.connector.connect(host=os.getenv('host'),user=os.getenv('user'),password=os.getenv('password'),database=os.getenv('database'))
+conn.commit = True
+dbcursor = conn.cursor(buffered=True)
+
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
