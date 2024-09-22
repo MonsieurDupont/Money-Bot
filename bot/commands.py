6d5b219dbcf2bbcc678eb512bbfa6ace5da04191
Samy <110028859+Dystilest@users.noreply.github.com>
2024-09-22 10:53:43 +0100

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -5,23 +5,13 @@ from discord.interactions import Interaction
 @app_commands.command(name="bonjour", description="Dire bonjour!")
 async def bonjour(interaction: Interaction):
     try:
-        # Répondre correctement à l'interaction en utilisant send_message
         await interaction.response.send_message(f'Bonjour, {interaction.user.mention}!')
     except Exception as e:
         print(f"Erreur: {e}")
 
-""" @app_commands.command(name="bye", description="Dire au bye!")
+@app_commands.command(name="bye", description="Dire au revoir!")
 async def bye(interaction: Interaction):
-    try:
-        # Répondre correctement à l'interaction en utilisant send_message
-        await interaction.response.send_message(f'bye, {interaction.user.mention}!')
-    except Exception as e:
-        print(f"Erreur: {e}")
- """
-
-@app_commands.command(name="bye", description="Dire au bye!")
-async def button(ctx):
-    button = Button(label='test', style=discord.ButtonStyle.red)
-    View = View()
+    button = Button(label='Test', style=discord.ButtonStyle.red)
+    view = View()
     view.add_item(button)
-    await ctx.send("This is a button!", view=View) # Send a message with our View class that contains the button       
+    await interaction.response.send_message("Voici un bouton !", view=view)
