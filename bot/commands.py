86dab283a3b9a55f3b0815da3cca8ba2c041312c
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-09-22 23:47:09 +0100

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from discord import app_commands, Interaction
-""" from discord.ui import Button, View """
+from discord.ui import Button, View
 
 class BotCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -10,13 +10,13 @@ class BotCommands(commands.Cog):
     async def bonjour(self, interaction: Interaction):
         await interaction.response.send_message(f'Bonjour {interaction.user.mention}!')
 
-"""     @app_commands.command(name="bye", description="Say goodbye!")
+     @app_commands.command(name="bye", description="Say goodbye!")
     async def bye_command(self, interaction: Interaction):
         button = Button(label="test", style=discord.ButtonStyle.green)
         view = View()
         view.add_item(button)
         await interaction.response.send_message(f'Bye {interaction.user.mention}!') 
- """
+
 # Async cog setup function
 async def setup(bot: commands.Bot):
     await bot.add_cog(BotCommands(bot))
