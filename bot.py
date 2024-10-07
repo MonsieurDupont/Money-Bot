fadd7ef24d92b1db19fe64f524a98e7cbe95ce3e
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-10-07 23:54:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,11 +100,12 @@ async def register(interaction: discord.Interaction):
 class TestButton(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
+
     @discord.ui.button(label="zebi", style=discord.ButtonStyle.blurple)
     async def buttontest(self, interaction: discord.Interaction, Button: discord.ui.Button):
         await interaction.channel.send(content="zebiiiiii")
     @discord.ui.button(label="zebi2", style=discord.ButtonStyle.danger)
-    async def buttontest(self, interaction: discord.Interaction, Button: discord.ui.Button):
+    async def buttontest2(self, interaction: discord.Interaction, Button: discord.ui.Button):
         await interaction.channel.send(content="grrrr")
 
 @bot.tree.command(name="balance", description="Vérifier votre solde")
