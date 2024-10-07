b7e286921c1c8da5bf2cfe2df4ef8f7fa3a52f6c
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-10-07 21:55:08 +0100

    Create .env

--- bot.py
+++ bot.py
@@ -105,7 +105,10 @@ async def balance(interaction: discord.Interaction):
     if data:
         cash, bank = data[0]
         total = cash + bank
-        embed = discord.Embed(title="Solde", description=f"Votre solde actuel : {cash} <:AploucheCoin:1286080674046152724> en cash, {bank} <:AploucheCoin:1286080674046152724> en banque, pour un total de {total} <:AploucheCoin:1286080674046152724>.", color=0x00ff00)
+        embed = discord.Embed(title="Solde", description=f" 
+        Cash : {cash} <:AploucheCoin:1286080674046152724>
+        Banque : {bank} <:AploucheCoin:1286080674046152724>
+        Total : {total} <:AploucheCoin:1286080674046152724>.", color=0x00ff00)
         await interaction.response.send_message(embed=embed)
     else:
         embed = discord.Embed(title="Erreur", description="Erreur lors de la récupération de vos données.", color=0xff0000)
