d2f7f9c35cc21121df7f50500516e68de2038802
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-10-09 18:23:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -535,7 +535,7 @@ async def leaderboard(interaction: discord.Interaction):
         user = bot.get_user(user_id)
         if user is None:
             continue
-        await interaction.response.send_message(f"#{i} {user.display_name} {total} ")
+        await interaction.channel.send(f"#{i} {user.display_name} {total} ")
         #embed.add_field(name=f"#{i}", value=f"{user.display_name} - {total:,} <:AploucheCoin:1286080674046152724>", inline=False)
     # embed.set_footer(text="Note : Ce classement est mis à jour en temps réel.")
     # await interaction.response.send_message(embed=embed)
