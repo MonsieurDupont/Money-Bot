2b3f92f625f711624ed3ed3a9996a57de8ca5374
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-10-10 23:25:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -263,7 +263,7 @@ async def balance(interaction: discord.Interaction, user: typing.Optional[discor
 
     total = cash + bank
     foo = await bot.fetch_user(user_id)
-    embed = discord.Embed(title=f"Solde de <@{foo.id}>", description=f"**Cash** : {cash:,} {CoinEmoji}\n**Banque** : {bank:,} {CoinEmoji}\n**Total** : {total:,} {CoinEmoji}", color=color_blue)
+    embed = discord.Embed(title=f"Solde de {foo.display_name}", description=f"**Cash** : {cash:,} {CoinEmoji}\n**Banque** : {bank:,} {CoinEmoji}\n**Total** : {total:,} {CoinEmoji}", color=color_blue)
     if total < 0:
         embed.add_field(name="", value="Wesh c'est la hess la ", inline=False)
     # embed.add_field(name="Aide", value="Pour voir les commandes disponibles, tapez `/help`.", inline=False)
@@ -651,7 +651,7 @@ async def transaction_history(interaction: discord.Interaction, user: typing.Opt
         await interaction.response.send_message(embed=embed)
         return
     foo = await bot.fetch_user(user_id)
-    embed = discord.Embed(title=f"Historique de <@{foo.id}>", description=f"Voici la liste des 10 dernieres transactions <@{foo.id}>:", color=color_blue)
+    embed = discord.Embed(title=f"Historique de {foo.display_name}", description="Voici la liste des 10 dernieres transactions :", color=color_blue)
     embed.add_field(name="", value="**Montant** | **Type**", inline=False)
     for i, (transaction_id, amount, transaction_type) in enumerate(transactions[::-1][:10], start=1):
         embed.add_field(name="", value=f"**{i}** : {amount:,} {CoinEmoji} | {transaction_type}", inline=False)
