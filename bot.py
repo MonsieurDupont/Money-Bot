74e7219af2207f22a4e62b1a1abea93672519d2d
Samy <110028859+Dystilest@users.noreply.github.com>
2024-10-11 14:38:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,13 @@ bot = commands.Bot(command_prefix="/", intents=discord.Intents.default())
 async def on_ready():
     print(f"Logged in as {bot.user.name} (ID: {bot.user.id})")
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Commande inconnue.")
+    else:
+        await ctx.send("Erreur lors de l'exécution de la commande.")
+
 async def main():
     await bot.start(TOKEN)
 
