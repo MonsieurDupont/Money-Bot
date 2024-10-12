bc465d7ca4494d94b57f32317dd964a086f4d821
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-10-12 16:59:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1107,6 +1107,7 @@ async def poker(interaction: discord.Interaction, mise: int):
     board = deck.draw(5)
     p1 = deck.draw(2)
     p2 = deck.draw(2)
+    print(p1)
     embed = discord.Embed(title="Poker", description=f"Vos cartes : {p1}", color=color_green)
     await interaction.response.send_message(embed=embed)
 
