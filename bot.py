d35b55cc0493c6ffc86ed6f3fe94a80bd4a82b98
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-10-16 20:25:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1282,7 +1282,7 @@ async def blackjack(interaction: discord.Interaction, amount: int):
     blackjack_players.append(user_id)
     view = BlackJackView()
     embed = discord.Embed(title=f"t", description=f"", color=color_blue)
-    embed.set_thumbnail(url=interaction.user.avatar.url)
+    embed.set_image(url=interaction.user.avatar.url)
     await interaction.response.send_message(embed=embed, view=view)
 
 if __name__ == "__main__":
