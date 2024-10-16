0b5ee31587effc8064d6c5312b8f9258b6068fb8
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-10-16 21:52:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1305,7 +1305,7 @@ async def blackjack(interaction: discord.Interaction, amount: int):
     embed = discord.Embed(title="", description=f"", color=color_blue)
     embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
     # embed.add_field(name=result, value="")
-    embed.add_field(name="Votre main", value=f"{player_cards}")
+    embed.add_field(name="Votre main", value=f"".join([card_to_emoji(card[0]) for card in player_cards]))
     embed.add_field(name=f"", value="")
     embed.add_field(name="Main du croupier", value=f"{dealer_cards[0][0]}")
     embed.add_field(name=f"", value="")
