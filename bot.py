0ed13ca78c372b43a7146a975d3d97343b0654f9
MonsieurDupont <122630849+MonsieurDupont@users.noreply.github.com>
2024-10-17 20:12:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1356,10 +1356,10 @@ async def blackjack(interaction: discord.Interaction, amount: int):
     
     # Cartes initiales du joueur
     embed.add_field(name="Vous", value=f"".join([card_to_emoji(Card.int_to_str(card)) for card in player_cards]))
-    embed.add_field(name="", value=f"{blackjack_sessions[user_id].evaluate_hand(player_cards)}")
+    embed.add_field(name="Total : ", value=f"{blackjack_sessions[user_id].evaluate_hand(player_cards)}")
     # Cartes initiales du croupier
     embed.add_field(name="Croupier", value=f"{card_to_emoji(Card.int_to_str(dealer_cards[0]))} {card_back}")
-    embed.add_field(name="", value=f"{blackjack_sessions[user_id].evaluate_hand(dealer_cards)}")
+    embed.add_field(name="Total : ", value=f"{blackjack_sessions[user_id].evaluate_hand(dealer_cards)}")
 
     await interaction.response.send_message(embed=embed, view=view)
 
